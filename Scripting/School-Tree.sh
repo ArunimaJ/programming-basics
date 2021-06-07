26db461a464facf0a4c0fb538c04036e412ddf19
Nirmal <nirmalk.web@gmail.com>
2021-06-07 11:31:59 +0530

    added for loop folder example

--- Scripting/School-Tree.sh
+++ Scripting/School-Tree.sh
@@ -1,10 +1,23 @@
  # Building a folder tree based on user input
  # ask the user city name, school name,grade number ,sections,languages 
  # Gurgaon / DPS / Grade-1 / Section-1 / French 
- Countries="India,Australia,Singapore" 
- read -p "Country name?" country_name
+ countries=(India Australia Singapore)
+ 
+ read -p "Tree name?" tree_name
  read -p "School name?" school_name
  read -p "Grade number?" grade_no
  read -p "Sections?" section
  read -p "Languages?" language_name
- mkdir -p ${country_name}/${city_name}/School-${school_name}/Grade-${grade_no}/Section-${section}/Language-$language_name
+ 
+ mkdir tree_name
+
+ for countryPickerIndex in "${!countries[@]}"
+ do
+
+  echo "Creating Directory for ${countries[$countryPickerIndex]}"
+  mkdir -p ${tree_name}/${countries[$countryPickerIndex]}/School-${school_name}/Grade-${grade_no}/Section-${section}/Language-$language_name
+
+ done
+
+
+ 
